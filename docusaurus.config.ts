d484234d580ce747de6db5850a6605dfd686960e
mariopepe <scugnzz@gmail.com>
2024-03-06 02:23:26 +0100

    Amended website name

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "Vimen",
+  tagline: "Fast cheap links",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
@@ -69,9 +69,9 @@ const config: Config = {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
     navbar: {
-      title: "My Site",
+      title: "Vimen",
       logo: {
-        alt: "My Site Logo",
+        alt: "Vimen Logo",
         src: "img/logo.svg",
         href: "/docs/category/getting-started",
       },
